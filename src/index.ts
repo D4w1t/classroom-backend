c9d965ff4b2d60b40abe9bacf46bb9b900bdbc5b
Dawit Nigussie <dawit9n@gmail.com>
2026-01-19 14:23:16 +0300

    Fixes

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import securityMiddleware from "./middleware/security";
 import { auth } from "./lib/auth";
 
 const app = express();
-const PORT = 8080;
+const PORT = Number(process.env.PORT ?? 8080);
 
 if (!process.env.FRONTEND_URL)
   throw new Error("FRONTEND_URL is required in environment variables");
@@ -39,8 +39,8 @@ app.get("/", (req, res) => {
   res.send("Hello, Welcome to Classroom API");
 });
 
+app.use("/api/v1/subjects", subjectsRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.use("/api/v1/subjects", subjectsRouter);
