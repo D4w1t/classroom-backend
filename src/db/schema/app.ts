5ed75dd2cf741e5a17c398a5e51bbd9ee5323ccf
Dawit Nigussie <dawit9n@gmail.com>
2026-01-19 11:03:28 +0300

    feat: add classes and enrollments tables with relationships, enums, and schema updates

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -1,5 +1,23 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  index,
+  integer,
+  jsonb,
+  pgEnum,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  unique,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { user } from "./auth";
+
+export const classStatusEnum = pgEnum("classStatus", [
+  "active",
+  "inactive",
+  "completed",
+]);
 
 const timestamps = {
   createdAt: timestamp("createdAt").defaultNow().notNull(),
@@ -28,6 +46,53 @@ export const subjects = pgTable("subjects", {
   ...timestamps,
 });
 
+export const classes = pgTable(
+  "classes",
+  {
+    id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+    subjectId: integer("subject_id")
+      .notNull()
+      .references(() => subjects.id, { onDelete: "cascade" }),
+    teacherId: text("teacher_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "restrict" }),
+    inviteCode: text("invite_code").notNull().unique(),
+    name: varchar("name", { length: 255 }).notNull(),
+    bannerCldPubId: text("banner_cld_pub_id"),
+    bannerUrl: text("banner_url"),
+    description: text("description"),
+    capacity: integer("capacity").notNull().default(30),
+    status: classStatusEnum("status").default("active").notNull(),
+    schedules: jsonb("schedules").$type<any[]>().default([]).notNull(),
+    ...timestamps,
+  },
+  (table) => [
+    index("classes_subject_id_idx").on(table.subjectId),
+    index("classes_teacher_id_idx").on(table.teacherId),
+  ],
+);
+
+export const enrollments = pgTable(
+  "enrollments",
+  {
+    studentId: text("student_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+    classId: integer("class_id")
+      .notNull()
+      .references(() => classes.id, { onDelete: "cascade" }),
+  },
+  (table) => [
+    primaryKey({ columns: [table.studentId, table.classId] }),
+    unique("enrollments_student_id_class_id_unique").on(
+      table.studentId,
+      table.classId,
+    ),
+    index("enrollments_student_id_idx").on(table.studentId),
+    index("enrollments_class_id_idx").on(table.classId),
+  ],
+);
+
 // One department has many subjects
 export const departmentRelations = relations(departments, ({ many }) => ({
   subjects: many(subjects),
@@ -41,8 +106,42 @@ export const subjectRelations = relations(subjects, ({ one, many }) => ({
   }),
 }));
 
+// One class belongs to one subject
+// One class belongs to one teacher
+// One class has many enrollments
+export const classRelations = relations(classes, ({ one, many }) => ({
+  subjects: one(subjects, {
+    fields: [classes.subjectId],
+    references: [subjects.id],
+  }),
+  teachers: one(user, {
+    fields: [classes.teacherId],
+    references: [user.id],
+  }),
+  enrollments: many(enrollments),
+}));
+
+// One enrollment belongs to one student
+// One enrollment belongs to one class
+export const enrollmentRelations = relations(enrollments, ({ one }) => ({
+  students: one(user, {
+    fields: [enrollments.studentId],
+    references: [user.id],
+  }),
+  classes: one(classes, {
+    fields: [enrollments.classId],
+    references: [classes.id],
+  }),
+}));
+
 export type Department = typeof departments.$inferSelect;
 export type NewDepartment = typeof departments.$inferInsert;
 
 export type Subject = typeof subjects.$inferSelect;
 export type NewSubject = typeof subjects.$inferInsert;
+
+export type Class = typeof classes.$inferSelect;
+export type NewClass = typeof classes.$inferInsert;
+
+export type Enrollment = typeof enrollments.$inferSelect;
+export type NewEnrollment = typeof enrollments.$inferInsert;
---
+++ src/db/schema/auth.ts
@@ -0,0 +1,114 @@
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  index,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum("role", ["student", "teacher", "admin"]);
+
+const timestamps = {
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("update_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+};
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  emailVerified: boolean("email_verified").notNull(),
+  image: text("image"),
+  role: roleEnum("role").default("student").notNull(),
+  imageCldPubId: text("image_cld_pub_id"),
+  ...timestamps,
+});
+
+export const session = pgTable(
+  "session",
+  {
+    id: text("id").primaryKey(),
+    expiresAt: timestamp("expires_at").notNull(),
+    token: text("token").notNull().unique(),
+    ipAddress: text("ip_address"),
+    userAgent: text("user_agent"),
+    userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+    ...timestamps,
+  },
+  (table) => [index("session_user_id_idx").on(table.userId)],
+);
+
+export const account = pgTable(
+  "account",
+  {
+    id: text("id").primaryKey(),
+    accountId: text("account_id").notNull(),
+    providerId: text("provider_id").notNull(),
+    userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+    accessToken: text("access_token"),
+    refreshToken: text("refresh_token"),
+    idToken: text("id_token"),
+    accessTokenExpiresAt: timestamp("accces_token_expires_at"),
+    refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+    scope: text("scope"),
+    password: text("password"),
+    ...timestamps,
+  },
+  (table) => [index("account_user_id_idx").on(table.userId)],
+);
+
+export const verification = pgTable(
+  "verification",
+  {
+    id: text("id").primaryKey(),
+    identifier: text("identifier").notNull(),
+    value: text("value").notNull(),
+    expiresAt: timestamp("expires_at").notNull(),
+    ...timestamps,
+  },
+  (table) => [index("verification_identifer_idx").on(table.identifier)],
+);
+
+// One user has many sessions
+// One user has many accounts
+export const userRelations = relations(user, ({ many }) => ({
+  sessions: many(session),
+  accounts: many(account),
+}));
+
+// One session belongs to one user
+export const sessionRelations = relations(session, ({ one }) => ({
+  user: one(user, {
+    fields: [session.userId],
+    references: [user.id],
+  }),
+}));
+
+// One account belongs to one user
+export const accountRelations = relations(account, ({ one }) => ({
+  user: one(user, {
+    fields: [account.userId],
+    references: [user.id],
+  }),
+}));
+
+export type User = typeof user.$inferSelect;
+export type NewUser = typeof user.$inferInsert;
+
+export type Session = typeof session.$inferSelect;
+export type NewSession = typeof session.$inferInsert;
+
+export type Account = typeof account.$inferSelect;
+export type NewAccount = typeof account.$inferInsert;
+
+export type Verification = typeof verification.$inferSelect;
+export type NewVerification = typeof verification.$inferInsert;
