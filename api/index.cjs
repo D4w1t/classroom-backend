924d633fbe2f8ab06ae8fca378f9be4e5d52c5a3
Dawit Nigussie <dawit9n@gmail.com>
2026-02-01 16:27:20 +0300

    Fix for prod on koyeb

--- api/index.cjs
+++
@@ -1,7 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../dist/index.js');
-
-// dist/index.js exports the app as default for ESM; handle both default and named
-const expressApp = app && app.default ? app.default : app;
-
-module.exports = serverless(expressApp);
